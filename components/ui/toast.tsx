15a13ee47d195b674b304833be98f9b6cae41d09
dj21-vicky21 <djvicky141@gmail.com>
2025-04-14 19:24:18 +0530

    Update ToastClose component to replace SVG icon with a text span for improved accessibility

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -83,7 +83,9 @@ const ToastClose = React.forwardRef<
     toast-close=""
     {...props}
   >
-    <X className="h-4 w-4" />
+   <span className='relative size-4'>
+    X
+   </span>
   </ToastPrimitives.Close>
 ));
 ToastClose.displayName = ToastPrimitives.Close.displayName;
